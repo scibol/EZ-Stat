525cc24017ae61685510283317a4071235ba4d9c
Paolo Aurecchia <Paolo@paolos-mbp.mobile.usilu.net>
2015-12-04 15:44:42 +0100

    socket stuff

--- app/js/socket.js
+++ app/js/socket.js
@@ -4,7 +4,12 @@ socket.on("change-game", function(data) {
     drawHit()
 });
 
+socket.on("change-room", function(data) {
+    console.log("Asd");
+    console.log(data)
+});
+
 socket.on("change-player", function(data) {
-    console.log("in")
+    console.log(socket);
     drawHitAndMiss(data.success, data.pos_x, data.pos_y)
 });
--- socket-server.js
+++ socket-server.js
@@ -1,6 +1,6 @@
 var socketIo = require('socket.io');
-
 var eventBus = require('./pubsub');
+//var room;
 
 module.exports = function(httpServer) {
 	var io = socketIo(httpServer);
@@ -15,15 +15,35 @@ module.exports = function(httpServer) {
 
 		socket.on('error', function(err){
 			console.log("Error: " + err)
-		})
+		});
+
+		socket.on("change", function(data){
+			//for (var i = 0; i < socket.rooms.length - 1; i++) {
+			//	socket.leave(socket.rooms[i]);
+			//}
+			socket.rooms = [];
+			socket.join(data);
+			console.log(data);
+			//console.log(socket.rooms)
+
+		});
+
 	});
 
 	eventBus.on('game.created', function(event){
-		io.emit('change-game', event);
+		var room = window.location.href.split("/games/")[1];
+		io.to(room).emit('change-game', event);
+	});
+
+	eventBus.on('change.room', function(event){
+		io.emit('change-room', event);
 	});
 
 	eventBus.on('player.changed', function(event){
-		io.emit('change-player', event);
+		var room = event.url;
+		var data = event.data;
+		console.log(room);
+		io.to(room).emit('change-player', data);
 	});
 
 	eventBus.on('track.deleted', function(event){
