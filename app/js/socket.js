fd83f651a45baad9f018ebe65baddeec444cd3bc
Luca Scibona <luca.scibona@usi.ch>
2015-12-12 01:20:15 +0100

    added socket for shots. goodnight

--- app/js/socket.js
+++ app/js/socket.js
@@ -1,31 +1,31 @@
 var socket = io.connect();
 
 
-socket.on("change-player", function (data) {
-
-    var ezApp = document.querySelector("ez-app");
-    var stats = ezApp.$.buttonCounter;
-    var canvas = ezApp.$.canv;
-    //
-    stats.$.getPlayer.generateRequest();
-    //
-    drawHitAndMiss(data.success, data.pos_x, data.pos_y, canvas.stage, canvas.selected, canvas.shotsObj)
-});
-
-socket.on("update-score", function (url) {
-    var ezApp = document.querySelector("ez-app");
-    var players = ezApp.game.players
-    for (var player in players) {
-        if (players[player]._id == url) {
-            var score = ezApp.$.canv;
-            //
-            var getScore = score.$.getTeamScore;
-            getScore.generateRequest();
-            //
-            score.drawScore()
-        }
-    }
-});
+//socket.on("change-player", function (data) {
+//
+//    var ezApp = document.querySelector("ez-app");
+//    var stats = ezApp.$.buttonCounter;
+//    var canvas = ezApp.$.canv;
+//    //
+//    stats.$.getPlayer.generateRequest();
+//    //
+//    drawHitAndMiss(data.success, data.pos_x, data.pos_y, canvas.stage, canvas.selected, canvas.shotsObj)
+//});
+//
+//socket.on("update-score", function (url) {
+//    var ezApp = document.querySelector("ez-app");
+//    var players = ezApp.game.players
+//    for (var player in players) {
+//        if (players[player]._id == url) {
+//            var score = ezApp.$.canv;
+//            //
+//            var getScore = score.$.getTeamScore;
+//            getScore.generateRequest();
+//            //
+//            score.drawScore()
+//        }
+//    }
+//});
 
 socket.on("change-state", function (data) {
     var ezApp = document.querySelector("ez-app");
@@ -48,6 +48,17 @@ socket.on("update-score", function (event) {
         }
     }
 });
+
+socket.on("player-update-shot", function (data) {
+    var ezApp = document.querySelector("ez-app");
+    var canvas = ezApp.$.canv;
+    if (data.type == "hit") {
+        canvas.drawHit(data.x, data.y)
+    }
+    else if (data.type == "miss") {
+        canvas.drawMiss(data.x, data.y)
+    }
+})
 
 
 socket.on("player-update-stat", function (data) {
